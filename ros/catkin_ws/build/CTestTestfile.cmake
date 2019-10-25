fb1b398cddbc529b005bf23b12111238242e3903
Joshua Riddell <jridd30@gmail.com>
2019-10-26 01:19:05 +1000

    Add ROS basic setup

---
+++ ros/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/josh/megabot/ros/catkin_ws/src
+# Build directory: /home/josh/megabot/ros/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("madbot_description")
+subdirs("madbot_gazebo")
