afb8414f7cb12c6967a5b89ab811d7d4ae09267a
Joshua Riddell <jridd30@gmail.com>
2019-10-26 14:07:56 +1000

    Add accurate arena model to robot simulation

--- ros/catkin_ws/build/catkin_generated/order_packages.py
+++ ros/catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/josh/megabot/ros/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/melodic".split(';') if "/opt/ros/melodic" != "" else []
+underlay_workspaces = "/home/josh/megabot/ros/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/josh/megabot/ros/catkin_ws/devel;/opt/ros/melodic" != "" else []
