2167988a85334288982a58a33313376b928b816c
Joshua Riddell <jridd30@gmail.com>
2020-01-25 07:02:47 +1000

    Add localisation node

--- ros/catkin_ws/src/localisation/src/localisation_node.cpp
+++ ros/catkin_ws/src/localisation/src/localisation_node.cpp
@@ -7,9 +7,10 @@
 #include <geometry_msgs/Twist.h>
 
 tf2::Transform increment;
+std::vector<tf2::Quaternion> map_line_angles;
+std::vector<tf2::Vector3> map_line_origins;
 
 void cmd_vel_callback(const geometry_msgs::Twist& msg) {
-
     tf2::Quaternion q;
 
     q.setRPY(
@@ -26,6 +27,40 @@ void cmd_vel_callback(const geometry_msgs::Twist& msg) {
     ));
 }
 
+void line_callback(const geometry_msgs::PoseStamped &msg) {
+    tf2::Quaternion line_angle;
+    tf2::convert(msg.pose.orientation, line_angle);
+
+    auto map_line_angle_it = map_line_angles.begin();
+    double minimum_angle = 999;
+    int closest_index = -1;
+
+    for (; map_line_angle_it != map_line_angles.end(); ++map_line_angle_it) {
+        double angle = map_line_angle_it->angleShortestPath(line_angle);
+        
+        // check if the other direction is a shorter angle
+        if (M_PI - angle < angle) {
+            angle = M_PI - angle;
+        }
+
+        if (angle < minimum_angle) {
+            closest_index = map_line_angle_it - map_line_angles.begin();
+            minimum_angle = angle;
+        }
+    }
+
+    if (minimum_angle > M_PI_4) {
+        return;
+    }
+
+    tf2::Quaternion map_line_angle = map_line_angles[closest_index];
+    tf2::Vector3 map_line_origin = map_line_origins[closest_index];
+
+    
+
+    std::cout << closest_index << ", " << minimum_angle << std::endl;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "localisation");
@@ -35,10 +70,24 @@ int main(int argc, char **argv)
     ros::Rate r(20);
 
     ros::Subscriber sub = n.subscribe("/cmd_vel", 1, cmd_vel_callback);
+    ros::Subscriber line_sub = n.subscribe("line", 1, line_callback);
 
     geometry_msgs::TransformStamped transformStamped;
     transformStamped.header.frame_id = "map";
     transformStamped.child_frame_id = "base_link";
+
+    // load in lines on the map
+    tf2::Quaternion q_tmp;
+    q_tmp.setRPY(0, 0, M_PI/2);
+    map_line_angles.push_back(q_tmp);
+    q_tmp.setRPY(0, 0, -M_PI/4);
+    map_line_angles.push_back(q_tmp);
+    q_tmp.setRPY(0, 0, -3*M_PI/4);
+    map_line_angles.push_back(q_tmp);
+
+    map_line_origins.push_back(tf2::Vector3(1.2, 0, 0));
+    map_line_origins.push_back(tf2::Vector3(0, 2.4, 0));
+    map_line_origins.push_back(tf2::Vector3(2.4, 0, 0));
 
     // setup initial state transform
     tf2::Transform state;
