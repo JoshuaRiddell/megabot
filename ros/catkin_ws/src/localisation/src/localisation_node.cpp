0ef1f4872320bd9ca94d012c0d96121205693613
Joshua Riddell <jridd30@gmail.com>
2020-01-27 08:02:01 +1000

    Closer to localisation via lines

--- ros/catkin_ws/src/localisation/src/localisation_node.cpp
+++ ros/catkin_ws/src/localisation/src/localisation_node.cpp
@@ -6,8 +6,9 @@
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Twist.h>
 
+tf2::Transform state;
 tf2::Transform increment;
-std::vector<tf2::Quaternion> map_line_angles;
+std::vector<tf2::Quaternion> map_line_quats;
 std::vector<tf2::Vector3> map_line_origins;
 
 void cmd_vel_callback(const geometry_msgs::Twist& msg) {
@@ -28,15 +29,18 @@ void cmd_vel_callback(const geometry_msgs::Twist& msg) {
 }
 
 void line_callback(const geometry_msgs::PoseStamped &msg) {
-    tf2::Quaternion line_angle;
-    tf2::convert(msg.pose.orientation, line_angle);
+    tf2::Quaternion line_quat;
+    tf2::convert(msg.pose.orientation, line_quat);
 
-    auto map_line_angle_it = map_line_angles.begin();
+    tf2::Vector3 line_position;
+    tf2::convert(msg.pose.position, line_position);
+
+    auto map_line_angle_it = map_line_quats.begin();
     double minimum_angle = 999;
     int closest_index = -1;
 
-    for (; map_line_angle_it != map_line_angles.end(); ++map_line_angle_it) {
-        double angle = map_line_angle_it->angleShortestPath(line_angle);
+    for (; map_line_angle_it != map_line_quats.end(); ++map_line_angle_it) {
+        double angle = map_line_angle_it->angleShortestPath(line_quat);
         
         // check if the other direction is a shorter angle
         if (M_PI - angle < angle) {
@@ -44,21 +48,73 @@ void line_callback(const geometry_msgs::PoseStamped &msg) {
         }
 
         if (angle < minimum_angle) {
-            closest_index = map_line_angle_it - map_line_angles.begin();
+            closest_index = map_line_angle_it - map_line_quats.begin();
             minimum_angle = angle;
         }
     }
 
+    tf2::Quaternion map_line_quat = map_line_quats[closest_index];
+    tf2::Vector3 map_line_origin = map_line_origins[closest_index];
+
+    // work in y/x gradients since most lines are close to vertical
+    double _, map_angle, line_angle;
+
+    tf2::Matrix3x3(map_line_quat).getRPY(_, _, map_angle);
+    double m1_inv = tan(M_PI_2 - map_angle);
+    double xc1 = map_line_origin.getX() - map_line_origin.getY() * m1_inv;
+
+    tf2::Matrix3x3(line_quat).getRPY(_, _, line_angle);
+    double m2_inv = tan(M_PI_2 - line_angle);
+    double xc2 = line_position.getX() - line_position.getY() * m2_inv;
+
+    double y = (xc2 - xc1) / (m1_inv - m2_inv);
+    double x = y * m1_inv + xc1;
+
+    double angle_diff = map_angle - line_angle;
+    if (angle_diff > M_PI_2) {
+        angle_diff = M_PI - angle_diff;
+    }
+    
+    tf2::Quaternion q;
+    q.setRPY(0,0,angle_diff);
+
+    tf2::Vector3 tmp = state.getOrigin() - tf2::Vector3(x,y,0);
+    tf2::Matrix3x3 rot;
+    rot.setRPY(0,0,angle_diff);
+    tf2::Vector3 tmp2 = rot * tmp;
+    
+    tf2::Transform inc;
+    tf2::Vector3 diff = tmp2 - tmp;
+    inc.setOrigin(diff);
+    inc.setRotation(q);
+
+    std::cout << closest_index << "," << angle_diff <<
+            // "; x1=y*" << m1_inv << "+" << xc1 <<
+            "; x2=y*" << m2_inv << "+" << xc2 <<
+            "; " << diff.getX() << "," << diff.getY() <<
+            "; " << state.getOrigin().getX() << "," << state.getOrigin().getY();
+            // "; int: " << x << "," << y << 
+            // std::endl;
+
+    // if the angle difference is too large this method becomes unstable
     if (minimum_angle > M_PI_4) {
+        std::cout << std::endl;
         return;
     }
 
-    tf2::Quaternion map_line_angle = map_line_angles[closest_index];
-    tf2::Vector3 map_line_origin = map_line_origins[closest_index];
+    // if minimum angle is too small we're already close enough
+    if (minimum_angle < 0.05) {
+        std::cout << std::endl;
+        return;
+    }
 
-    
+    state *= inc;
 
-    std::cout << closest_index << ", " << minimum_angle << std::endl;
+    std::cout << 
+            "; " << state.getOrigin().getX() << "," << state.getOrigin().getY() <<
+            std::endl;
+
+
 }
 
 int main(int argc, char **argv)
@@ -79,18 +135,17 @@ int main(int argc, char **argv)
     // load in lines on the map
     tf2::Quaternion q_tmp;
     q_tmp.setRPY(0, 0, M_PI/2);
-    map_line_angles.push_back(q_tmp);
+    map_line_quats.push_back(q_tmp);
     q_tmp.setRPY(0, 0, -M_PI/4);
-    map_line_angles.push_back(q_tmp);
+    map_line_quats.push_back(q_tmp);
     q_tmp.setRPY(0, 0, -3*M_PI/4);
-    map_line_angles.push_back(q_tmp);
+    map_line_quats.push_back(q_tmp);
 
     map_line_origins.push_back(tf2::Vector3(1.2, 0, 0));
     map_line_origins.push_back(tf2::Vector3(0, 2.4, 0));
     map_line_origins.push_back(tf2::Vector3(2.4, 0, 0));
 
     // setup initial state transform
-    tf2::Transform state;
     tf2::Quaternion q;
     q.setRPY(0, 0, M_PI_2);
     state.setOrigin(tf2::Vector3(1.2, 0.265, 0));
