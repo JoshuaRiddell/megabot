b2138a15e0ad8b5933477a1829d24782c92a1ac1
Joshua Riddell <jridd30@gmail.com>
2020-08-09 13:52:41 +1000

    Add LCD on display topic

--- lifter_arduino/lifter_arduino.ino
+++ lifter_arduino/lifter_arduino.ino
@@ -1,15 +1,12 @@
 #include <ros.h>
 #include <Servo.h>
+#include <LiquidCrystal_I2C.h>
 
 #include <std_msgs/Int8.h>
 #include <std_msgs/Int16.h>
+#include <std_msgs/String.h>
 
 #include <TimerOne.h>
-
-// servo left and right uint8 message
-// up down uint8 message
-// emission up down uint8 message
-// string display message
 
 enum {
   STEPPER_DOWN = -1,
@@ -25,23 +22,29 @@ const int leftGripperStart = 90;
 const int rightGripperPin = 10;
 const int rightGripperStart = 90;
 
-const int stepperDirPin = 7;
-const int stepperStepPin = 8;
-const int stepperUpEndPin = 4;
-const int stepperDownEndPin = 5;
+const int stepperDirPin = A1;
+const int stepperStepPin = A2;
+const int stepperUpEndPin = 11;
+const int stepperDownEndPin = 12;
+const int stepperEnablePin = A3;
 int currentEndPin = stepperDownEndPin;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setupGrippers();
 void setupStepper();
+void setupLcd();
 
 void leftGripperCb(const std_msgs::Int16& msg);
 void rightGripperCb(const std_msgs::Int16& msg);
 void stepperCb(const std_msgs::Int16& msg);
+void displayCb(const std_msgs::String& msg);
 
 ros::NodeHandle nh;
 ros::Subscriber<std_msgs::Int16> leftGripperSub("lifter/servo_left", leftGripperCb);
 ros::Subscriber<std_msgs::Int16> rightGripperSub("lifter/servo_right", rightGripperCb);
 ros::Subscriber<std_msgs::Int16> stepperSub("lifter/stepper", stepperCb);
+ros::Subscriber<std_msgs::String> displaySub("display", displayCb);
 
 void leftGripperCb(const std_msgs::Int16& msg) {
   leftGripper.write(msg.data);
@@ -66,9 +69,14 @@ void stepperCb(const std_msgs::Int16& msg) {
   }
 }
 
+void displayCb(const std_msgs::String& msg) {
+  lcd.setCursor(0, 0);
+  lcd.print(msg.data);
+}
+
 void toggleStep() {
   if (!digitalRead(currentEndPin)) {
-    for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 2; ++i) {
       digitalWrite(stepperStepPin, !digitalRead(stepperStepPin));
       delayMicroseconds(100);
       digitalWrite(stepperStepPin, !digitalRead(stepperStepPin));
@@ -83,6 +91,7 @@ void setup() {
   nh.initNode();
   setupGrippers();
   setupStepper();
+  setupLcd();
 }
 
 void setupGrippers() {
@@ -99,14 +108,23 @@ void setupGrippers() {
 void setupStepper() {
   pinMode(stepperDirPin, OUTPUT);
   pinMode(stepperStepPin, OUTPUT);
+  pinMode(stepperEnablePin, OUTPUT);
   pinMode(stepperUpEndPin, INPUT);
   pinMode(stepperDownEndPin, INPUT);
 
   digitalWrite(stepperDirPin, LOW);
   digitalWrite(stepperStepPin, LOW);
+  digitalWrite(stepperEnablePin, LOW);
   currentEndPin = stepperUpEndPin;
 
   nh.subscribe(stepperSub);
+}
+
+void setupLcd() {
+  lcd.init();
+  lcd.backlight();
+  
+  nh.subscribe(displaySub);
 }
 
 void loop() {
